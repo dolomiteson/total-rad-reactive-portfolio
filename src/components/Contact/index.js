626d58618e5fcf6ae1c0cc8368a4e3147995e1c3
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-06-25 13:05:01 -0500

    Complete starter foe all Compnents

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section className="my-5">
+      <h1 id="contact">Contact:</h1>
+      <p>
+        Contact: Coming Soon! 
+      </p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <section className="my-5">
+      <h1 id="resume">Resume:</h1>
+      <p>
+        Resume: Coming Soon! 
+      </p>
+    </section>
+  );
+}
+
+export default Resume;
